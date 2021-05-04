2e373d607db1f788754eb4f1747e87d969429210
Harshith7016 <harshithece7016@gmail.com>
2021-05-05 03:12:37 +0530

    feat: history page added

--- src/pages/History.js
+++ src/pages/History.js
@@ -3,29 +3,24 @@ import { VideoCard } from '../components'
 import { useVideo } from '../context/VideoContext'
 
 export const History = () => {
-  const [isLoading, setIsLoading] = useState(false)
-  const { videoState, videoDispatch } = useVideo()
+  const { videoState } = useVideo()
   console.log(videoState)
   const listedVideos = videoState?.historyVideos
 
   return (
     <div>
-      {isLoading ? (
-        <div className='page__loader'></div>
-      ) : (
-        <>
-          <div className='page__title'>History</div>
-          <div className='video__container'>
-            {listedVideos.length === 0 ? (
-              <div>No Videos Found</div>
-            ) : (
-              listedVideos.map((video) => (
-                <VideoCard key={video._id} video={video} />
-              ))
-            )}
-          </div>
-        </>
-      )}
+      <>
+        <div className='page__title'>History</div>
+        <div className='video__container'>
+          {listedVideos.length === 0 ? (
+            <div>No Videos Found</div>
+          ) : (
+            listedVideos.map((video) => (
+              <VideoCard key={video._id} video={video} />
+            ))
+          )}
+        </div>
+      </>
     </div>
   )
 }
