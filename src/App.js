19e8152de795ca4bf7c6a704389c4dedc7a221b6
Harshith7016 <harshithece7016@gmail.com>
2021-05-05 17:15:41 +0530

    feat: home page added

--- src/App.js
+++ src/App.js
@@ -2,15 +2,14 @@ import './styles.css'
 import { Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import {
-  History,
   Home,
-  LikedVideos,
   PlayLists,
   SinglePlayList,
   VideoListing,
   VideoPlayer
 } from './pages'
-import { NavBar, SideNavBar } from './components'
+import { NavBar } from './components'
+import { Library } from './pages/Library'
 
 export default function App() {
   return (
@@ -19,22 +18,17 @@ export default function App() {
         <NavBar />
       </div>
       <ToastContainer />
-      <div className='container'>
-        <div className='sidenav-container'>
-          <SideNavBar />
-        </div>
-        <div className='component-container'>
-          <Routes>
-            <Route path='/' element={<VideoListing />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/videos' element={<VideoListing />} />
-            <Route path='/videos/:id' element={<VideoPlayer />} />
-            <Route path='/videos/liked' element={<LikedVideos />} />
-            <Route path='/videos/history' element={<History />} />
-            <Route path='/playlists' element={<PlayLists />} />
-            <Route path='/playlists/:id' element={<SinglePlayList />} />
-          </Routes>
-        </div>
+
+      <div className='component-container'>
+        <Routes>
+          <Route path='/' element={<VideoListing />} />
+          <Route path='/home' element={<Home />} />
+          <Route path='/videos' element={<VideoListing />} />
+          <Route path='/videos/:id' element={<VideoPlayer />} />
+          <Route path='/videos/library' element={<Library />} />
+          <Route path='/playlists' element={<PlayLists />} />
+          <Route path='/playlists/:id' element={<SinglePlayList />} />
+        </Routes>
       </div>
     </div>
   )
