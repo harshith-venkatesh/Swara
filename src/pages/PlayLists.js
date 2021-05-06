1cc0555a0557787ee229c98952d979835c877104
Harshith7016 <harshithece7016@gmail.com>
2021-05-06 05:50:52 +0530

    feat: My Playlist page created

--- src/pages/PlayLists.js
+++ src/pages/PlayLists.js
@@ -1,14 +1,39 @@
 import React from 'react'
+import { VideoCard } from '../components'
+import { useVideo } from '../context/VideoContext'
 
 export const PlayLists = () => {
+  const { videoState } = useVideo()
+  const { playlists } = videoState
   return (
     <div>
-      <div className='page__title'>
-        <div>My Playlist</div>
-      </div>
-      <div className='library__container'>
-        <div>Work Under Progress</div>
-      </div>
+      <>
+        <div className='page__title'>
+          <div>My Playlist</div>
+        </div>
+
+        {playlists.length === 0 ? (
+          <div>No Videos Found</div>
+        ) : (
+          playlists.map(({ _id, title, videosList }) => (
+            <div key={_id}>
+              <div className='page__title'>
+                <div>{title}</div>
+              </div>
+              <div className='library__container'>
+                {videosList.length === 0 ? (
+                  <div>No Videos found for the playlist</div>
+                ) : (
+                  videosList.map((video) => (
+                    <VideoCard key={video._id} video={video} />
+                  ))
+                )}
+              </div>
+              <hr />
+            </div>
+          ))
+        )}
+      </>
     </div>
   )
 }
